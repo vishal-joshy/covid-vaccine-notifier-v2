503a845f09d009a0776e124652f88a8d26690e02
VishalJoshy <1812-vishal@protonmail.com>
2021-09-02 13:16:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import HospitalList from './components/vaccines/HospitalList';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<HospitalList />
+		</div>
+	);
+}
+
+export default App;
