b24ca082d01fa8ea0a45e168ccea3cef6bbbc562
vishaljoshy18 <1812-vishal@protonmail.com>
2022-02-09 20:57:37 +0530

    Add check before rendering centers.

--- src/App.js
+++ src/App.js
@@ -6,56 +6,56 @@ import Header from './components/Header';
 import { getPresentDate } from './components/HelperModules/DateTime';
 
 function App() {
-	const [vaccineCenters, setVaccineCenters] = useState([]);
-	const [selectedDistrict, setSelectedDistrict] = useState({
-		district_id: 307,
-		district_name: 'Ernakulam',
-	});
+  const [vaccineCenters, setVaccineCenters] = useState(null);
+  const [selectedDistrict, setSelectedDistrict] = useState({
+    district_id: 307,
+    district_name: 'Ernakulam',
+  });
 
-	const getDataFromApi = async (url) => {
-		try {
-			const response = await fetch(url, {
-				mode: 'cors',
-			});
-			const apiData = await response.json();
-			const centerList = createVaccineCenterList([...apiData.centers]);
-			setVaccineCenters([...centerList]);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getDataFromApi = async (url) => {
+    try {
+      const response = await fetch(url, {
+        mode: 'cors',
+      });
+      const apiData = await response.json();
+      const centerList = createVaccineCenterList([...apiData.centers]);
+      setVaccineCenters([...centerList]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const getDistrict = (district) => {
-		setSelectedDistrict(district);
-	};
+  const getDistrict = (district) => {
+    setSelectedDistrict(district);
+  };
 
-	const getUrl = () => {
-		const district = selectedDistrict.district_id;
-		const date = getPresentDate();
-		const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district}&date=${date}`;
-		return url;
-	};
+  const getUrl = () => {
+    const district = selectedDistrict.district_id;
+    const date = getPresentDate();
+    const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district}&date=${date}`;
+    return url;
+  };
 
-	useEffect(() => {
-		//Call APi with Delay
+  useEffect(() => {
+    //Call APi with Delay
 
-		getDataFromApi(getUrl());
+    getDataFromApi(getUrl());
 
-		const inter = setInterval(() => {
-			getDataFromApi(getUrl());
-		}, 10000);
+    const inter = setInterval(() => {
+      getDataFromApi(getUrl());
+    }, 10000);
 
-		return () => {
-			clearInterval(inter);
-		};
-	}, [selectedDistrict]);
+    return () => {
+      clearInterval(inter);
+    };
+  }, [selectedDistrict]);
 
-	return (
-		<div className='App'>
-			<Header getDistrict={getDistrict} selectedDistrict={selectedDistrict} />
-			<VaccineCenterList vaccineCenters={vaccineCenters} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header getDistrict={getDistrict} selectedDistrict={selectedDistrict} />
+      {vaccineCenters && <VaccineCenterList vaccineCenters={vaccineCenters} />}
+    </div>
+  );
 }
 
 export default App;
